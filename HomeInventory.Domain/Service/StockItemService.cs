7d5612b0800bfd609f77b108ba7e84ac57b83330
Björn Hegstam <bjorn.hegstam@gmail.com>
2024-11-02 18:58:01 +0100

    Add method for adding stock item

--- HomeInventory.Domain/Service/StockItemService.cs
+++ HomeInventory.Domain/Service/StockItemService.cs
@@ -1,11 +1,26 @@
 using HomeInventory.Domain.Database;
 using HomeInventory.Domain.Model;
+using HomeInventory.Domain.Request;
 using Microsoft.EntityFrameworkCore;
 
 namespace HomeInventory.Domain.Service;
 
 public class StockItemService(StockItemContext stockItemContext)
 {
+    public async Task<StockItem> AddStockItem(AddStockItemRequest request)
+    {
+        var stockItem = new StockItem
+        {
+            StockItemId = StockItemId.New(),
+            Name = request.Name,
+            Sku = request.Sku,
+            InventoryCount = request.InventoryCount
+        };
+        await stockItemContext.StockItems.AddAsync(stockItem);
+
+        return stockItem;
+    }
+    
     public async Task<StockItem?> FindBySku(Sku sku)
     {
         return await stockItemContext.StockItems.FirstOrDefaultAsync(s => s.Sku == sku);
