96850da36c51649b9469369bc143018b6f509c9c
Björn Hegstam <bjorn.hegstam@gmail.com>
2025-04-27 14:06:52 +0200

    Manage package versions centrally

--- HomeInventory.WebApi/Program.cs
+++ HomeInventory.WebApi/Program.cs
@@ -3,56 +3,49 @@ using HomeInventory.Domain.Integration;
 using HomeInventory.Domain.Service;
 using Microsoft.EntityFrameworkCore;
 
-namespace HomeInventory.WebApi;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-public partial class Program
+IServiceCollection services = builder.Services;
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddControllers();
+
+services.AddScoped<IStockItemService, StockItemService>();
+services.AddScoped<IStockItemRepository, EfStockItemRepository>();
+
+services.AddDbContext<StockItemContext>(options =>
 {
-    public static void Main(string[] args)
+    string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
+
+services.AddCors(options =>
+{
+    if (builder.Environment.IsDevelopment())
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-        IServiceCollection services = builder.Services;
-
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-        services.AddControllers();
-
-        services.AddScoped<IStockItemService, StockItemService>();
-        services.AddScoped<IStockItemRepository, EfStockItemRepository>();
-
-        services.AddDbContext<StockItemContext>(options =>
-        {
-            string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            options.UseNpgsql(connectionString);
-        });
-
-        services.AddCors(options =>
-        {
-            if (builder.Environment.IsDevelopment())
-            {
-                options.AddDefaultPolicy(corsPolicyBuilder =>
-                    corsPolicyBuilder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
-                );
-            }
-        });
-
-        WebApplication app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.MapControllers();
-
-        app.Run();
+        options.AddDefaultPolicy(corsPolicyBuilder =>
+            corsPolicyBuilder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+        );
     }
+});
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
+// ReSharper disable once ClassNeverInstantiated.Global
 public partial class Program
 {
     // Required for integration tests
